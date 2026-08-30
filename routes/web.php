7654c407ecb0f216d11d22a66f63e05e98564e65
Philip Oyelegbin <philipoyelegbin@gmil.com>
2026-08-30 13:57:54 +0100

    Debug 500 error.

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [ArticleController::class, 'home'])->name('home');
 Route::get('/articles', [ArticleController::class, 'articles'])->name('articles');
 Route::post('/articles', [ArticleController::class, 'filter'])->name('articles.filter');
+Route::get('/debug', function () {
+    return response()->json([
+        'status' => 'Laravel is running',
+        'environment' => app()->environment(),
+        'debug' => config('app.debug'),
+        'laravel' => app()->version(),
+        'php' => PHP_VERSION,
+    ]);
+});
