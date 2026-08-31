0bf358c6e1c2802cbd3d512b86fbea496efb9c2c
Philip Oyelegbin <philipoyelegbin@gmil.com>
2026-08-31 12:29:48 +0100

    Fix the null data.

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -15,22 +15,54 @@ class ArticleController extends Controller
 
     public function home()
     {
-        if (Redis::exists("$this->cacheKey-home")) {
-            $result = json_decode(Redis::get("$this->cacheKey-home"), true);
-        } else {
-            $tech = Http::get("$this->apiUrl/articles?category=tech&limit=3");
-            $sport = Http::get("$this->apiUrl/articles?category=sports&limit=1");
-            $finance = Http::get("$this->apiUrl/articles?category=finance&limit=3");
-            $result = array_merge([
-                'tech' => $tech->json(),
-                'sports' => $sport->json(),
-                'finance' => $finance->json(),
-            ]);
-            Redis::set("$this->cacheKey-home", json_encode($result));
-        }
+        $tech = Http::get("$this->apiUrl/articles?category=tech&limit=3");
+        $sport = Http::get("$this->apiUrl/articles?category=sports&limit=1");
+        $finance = Http::get("$this->apiUrl/articles?category=finance&limit=3");
 
-        return view('welcome', compact('result'));
+        dd([
+            'apiUrl' => $this->apiUrl,
+
+            'tech' => [
+                'status' => $tech->status(),
+                'successful' => $tech->successful(),
+                'body' => $tech->body(),
+                'json' => $tech->json(),
+            ],
+
+            'sports' => [
+                'status' => $sport->status(),
+                'successful' => $sport->successful(),
+                'body' => $sport->body(),
+                'json' => $sport->json(),
+            ],
+
+            'finance' => [
+                'status' => $finance->status(),
+                'successful' => $finance->successful(),
+                'body' => $finance->body(),
+                'json' => $finance->json(),
+            ],
+        ]);
     }
+
+    // public function home()
+    // {
+    //     if (Redis::exists("$this->cacheKey-home")) {
+    //         $result = json_decode(Redis::get("$this->cacheKey-home"), true);
+    //     } else {
+    //         $tech = Http::get("$this->apiUrl/articles?category=tech&limit=3");
+    //         $sport = Http::get("$this->apiUrl/articles?category=sports&limit=1");
+    //         $finance = Http::get("$this->apiUrl/articles?category=finance&limit=3");
+    //         $result = array_merge([
+    //             'tech' => $tech->json(),
+    //             'sports' => $sport->json(),
+    //             'finance' => $finance->json(),
+    //         ]);
+    //         Redis::set("$this->cacheKey-home", json_encode($result));
+    //     }
+
+    //     return view('welcome', compact('result'));
+    // }
 
     public function articles()
     {
